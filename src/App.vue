4507f83888565a73d184baa2317efd4f89fd1142
Michiel Lankamp <michiel.lankamp@madaster.com>
2026-04-20 16:14:25 +0200

    feat: display package versions in app bar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import inf from '../package-lock.json';
+
 import Viewer from '@/components/viewer.vue';
 </script>
 
 <template>
   <v-app>
-    <v-app-bar title="Vue, typescript and Xeokit" />
+    <v-app-bar :title="`Vue (${inf.packages['node_modules/vue'].version}), typescript (${inf.packages['node_modules/typescript'].version}) and Xeokit (${inf.packages['node_modules/@xeokit/xeokit-sdk'].version})`" />
 
     <viewer />
     <v-icon />
